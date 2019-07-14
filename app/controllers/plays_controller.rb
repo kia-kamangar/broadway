0a1d7527244ef1e589077de6f135d0505a3cdef1
kia <kia.kamangar@gmail.com>
2019-07-14 18:33:00 +0430

    Edit and Delete actions and view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -21,6 +21,23 @@ class PlaysController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
+	def update
+		if @play.update(play_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @play.destroy
+			redirect_to root_path
+		end
+	end
+
 	private
 	def play_params
 		params.require(:play).permit(:title, :description, :director)
